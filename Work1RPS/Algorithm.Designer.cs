07662525ecaf7b8cadd52848e6ec64489219104d
lxst4 <146591561+lxst4@users.noreply.github.com>
2024-04-01 10:09:45 +0300

    add

--- Work1RPS/Algorithm.Designer.cs
+++ Work1RPS/Algorithm.Designer.cs
@@ -54,9 +54,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 16F);
-            label1.Location = new Point(236, 9);
+            label1.Location = new Point(206, 7);
             label1.Name = "label1";
-            label1.Size = new Size(380, 37);
+            label1.Size = new Size(314, 30);
             label1.TabIndex = 0;
             label1.Text = "Найти пересечение отрезков";
             label1.Click += label1_Click;
@@ -65,9 +65,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 14F);
-            label2.Location = new Point(12, 76);
+            label2.Location = new Point(10, 57);
             label2.Name = "label2";
-            label2.Size = new Size(123, 32);
+            label2.Size = new Size(99, 25);
             label2.TabIndex = 1;
             label2.Text = "Прямая 1:";
             // 
@@ -75,9 +75,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 14F);
-            label3.Location = new Point(12, 122);
+            label3.Location = new Point(10, 92);
             label3.Name = "label3";
-            label3.Size = new Size(248, 32);
+            label3.Size = new Size(195, 25);
             label3.TabIndex = 2;
             label3.Text = "Координаты точки A:";
             label3.Click += label3_Click;
@@ -86,9 +86,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 14F);
-            label4.Location = new Point(13, 171);
+            label4.Location = new Point(11, 128);
             label4.Name = "label4";
-            label4.Size = new Size(247, 32);
+            label4.Size = new Size(194, 25);
             label4.TabIndex = 3;
             label4.Text = "Координаты точки B:";
             // 
@@ -96,9 +96,9 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 14F);
-            label5.Location = new Point(12, 223);
+            label5.Location = new Point(10, 167);
             label5.Name = "label5";
-            label5.Size = new Size(123, 32);
+            label5.Size = new Size(99, 25);
             label5.TabIndex = 4;
             label5.Text = "Прямая 2:";
             // 
@@ -106,9 +106,9 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 14F);
-            label6.Location = new Point(12, 266);
+            label6.Location = new Point(10, 200);
             label6.Name = "label6";
-            label6.Size = new Size(248, 32);
+            label6.Size = new Size(195, 25);
             label6.TabIndex = 5;
             label6.Text = "Координаты точки C:";
             // 
@@ -116,9 +116,9 @@
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI", 14F);
-            label7.Location = new Point(12, 311);
+            label7.Location = new Point(10, 233);
             label7.Name = "label7";
-            label7.Size = new Size(250, 32);
+            label7.Size = new Size(196, 25);
             label7.TabIndex = 6;
             label7.Text = "Координаты точки D:";
             label7.Click += label7_Click;
@@ -126,45 +126,54 @@
             // pointAx1
             // 
             pointAx1.Font = new Font("Segoe UI", 14F);
-            pointAx1.Location = new Point(265, 119);
+            pointAx1.Location = new Point(232, 89);
+            pointAx1.Margin = new Padding(3, 2, 3, 2);
             pointAx1.Name = "pointAx1";
-            pointAx1.Size = new Size(92, 39);
+            pointAx1.PlaceholderText = "x1";
+            pointAx1.Size = new Size(81, 32);
             pointAx1.TabIndex = 7;
             pointAx1.TextChanged += pointAx1_TextChanged;
             // 
             // pointDx4
             // 
             pointDx4.Font = new Font("Segoe UI", 14F);
-            pointDx4.Location = new Point(265, 308);
+            pointDx4.Location = new Point(232, 231);
+            pointDx4.Margin = new Padding(3, 2, 3, 2);
             pointDx4.Name = "pointDx4";
-            pointDx4.Size = new Size(92, 39);
+            pointDx4.PlaceholderText = "x4";
+            pointDx4.Size = new Size(81, 32);
             pointDx4.TabIndex = 8;
             pointDx4.TextChanged += pointDx4_TextChanged;
             // 
             // pointCx3
             // 
             pointCx3.Font = new Font("Segoe UI", 14F);
-            pointCx3.Location = new Point(266, 263);
+            pointCx3.Location = new Point(233, 197);
+            pointCx3.Margin = new Padding(3, 2, 3, 2);
             pointCx3.Name = "pointCx3";
-            pointCx3.Size = new Size(92, 39);
+            pointCx3.PlaceholderText = "x3";
+            pointCx3.Size = new Size(81, 32);
             pointCx3.TabIndex = 9;
             pointCx3.TextChanged += pointCx3_TextChanged;
             // 
             // pointBx2
             // 
             pointBx2.Font = new Font("Segoe UI", 14F);
-            pointBx2.Location = new Point(266, 168);
+            pointBx2.Location = new Point(233, 126);
+            pointBx2.Margin = new Padding(3, 2, 3, 2);
             pointBx2.Name = "pointBx2";
-            pointBx2.Size = new Size(92, 39);
+            pointBx2.PlaceholderText = "x2";
+            pointBx2.Size = new Size(81, 32);
             pointBx2.TabIndex = 10;
             pointBx2.TextChanged += pointBx2_TextChanged;
             // 
             // Btn_GetResult
             // 
             Btn_GetResult.Font = new Font("Segoe UI", 14F);
-            Btn_GetResult.Location = new Point(68, 384);
+            Btn_GetResult.Location = new Point(10, 299);
+            Btn_GetResult.Margin = new Padding(3, 2, 3, 2);
             Btn_GetResult.Name = "Btn_GetResult";
-            Btn_GetResult.Size = new Size(194, 50);
+            Btn_GetResult.Size = new Size(170, 38);
             Btn_GetResult.TabIndex = 11;
             Btn_GetResult.Text = "Выполнить";
             Btn_GetResult.UseVisualStyleBackColor = true;
@@ -173,9 +182,10 @@
             // Btn_SaveOriginal
             // 
             Btn_SaveOriginal.Font = new Font("Segoe UI", 14F);
-            Btn_SaveOriginal.Location = new Point(496, 240);
+            Btn_SaveOriginal.Location = new Point(444, 89);
+            Btn_SaveOriginal.Margin = new Padding(3, 2, 3, 2);
             Btn_SaveOriginal.Name = "Btn_SaveOriginal";
-            Btn_SaveOriginal.Size = new Size(270, 48);
+            Btn_SaveOriginal.Size = new Size(236, 36);
             Btn_SaveOriginal.TabIndex = 12;
             Btn_SaveOriginal.Text = "Сохранить исходные данные";
             Btn_SaveOriginal.UseVisualStyleBackColor = true;
@@ -184,9 +194,10 @@
             // Btn_SaveResult
             // 
             Btn_SaveResult.Font = new Font("Segoe UI", 14F);
-            Btn_SaveResult.Location = new Point(496, 311);
+            Btn_SaveResult.Location = new Point(444, 145);
+            Btn_SaveResult.Margin = new Padding(3, 2, 3, 2);
             Btn_SaveResult.Name = "Btn_SaveResult";
-            Btn_SaveResult.Size = new Size(270, 48);
+            Btn_SaveResult.Size = new Size(236, 36);
             Btn_SaveResult.TabIndex = 13;
             Btn_SaveResult.Text = "Сохранить результат";
             Btn_SaveResult.UseVisualStyleBackColor = true;
@@ -195,9 +206,10 @@
             // Btn_GetFromFile
             // 
             Btn_GetFromFile.Font = new Font("Segoe UI", 14F);
-            Btn_GetFromFile.Location = new Point(496, 384);
+            Btn_GetFromFile.Location = new Point(444, 200);
+            Btn_GetFromFile.Margin = new Padding(3, 2, 3, 2);
             Btn_GetFromFile.Name = "Btn_GetFromFile";
-            Btn_GetFromFile.Size = new Size(270, 48);
+            Btn_GetFromFile.Size = new Size(236, 36);
             Btn_GetFromFile.TabIndex = 14;
             Btn_GetFromFile.Text = "Ввод данных из файла";
             Btn_GetFromFile.UseVisualStyleBackColor = true;
@@ -207,9 +219,9 @@
             // 
             result_label.AutoSize = true;
             result_label.Font = new Font("Segoe UI", 14F);
-            result_label.Location = new Point(535, 119);
+            result_label.Location = new Point(218, 299);
             result_label.Name = "result_label";
-            result_label.Size = new Size(121, 32);
+            result_label.Size = new Size(96, 25);
             result_label.TabIndex = 15;
             result_label.Text = "результат";
             result_label.Click += result_label_Click;
@@ -217,44 +229,52 @@
             // pointAy1
             // 
             pointAy1.Font = new Font("Segoe UI", 14F);
-            pointAy1.Location = new Point(382, 119);
+            pointAy1.Location = new Point(334, 89);
+            pointAy1.Margin = new Padding(3, 2, 3, 2);
             pointAy1.Name = "pointAy1";
-            pointAy1.Size = new Size(92, 39);
+            pointAy1.PlaceholderText = "y1";
+            pointAy1.Size = new Size(81, 32);
             pointAy1.TabIndex = 16;
             pointAy1.TextChanged += pointAy1_TextChanged_1;
             // 
             // pointDy4
             // 
             pointDy4.Font = new Font("Segoe UI", 14F);
-            pointDy4.Location = new Point(381, 308);
+            pointDy4.Location = new Point(333, 231);
+            pointDy4.Margin = new Padding(3, 2, 3, 2);
             pointDy4.Name = "pointDy4";
-            pointDy4.Size = new Size(92, 39);
+            pointDy4.PlaceholderText = "y4";
+            pointDy4.Size = new Size(81, 32);
             pointDy4.TabIndex = 17;
             pointDy4.TextChanged += pointDy4_TextChanged;
             // 
             // pointCy3
             // 
             pointCy3.Font = new Font("Segoe UI", 14F);
-            pointCy3.Location = new Point(381, 263);
+            pointCy3.Location = new Point(333, 197);
+            pointCy3.Margin = new Padding(3, 2, 3, 2);
             pointCy3.Name = "pointCy3";
-            pointCy3.Size = new Size(92, 39);
+            pointCy3.PlaceholderText = "y3";
+            pointCy3.Size = new Size(81, 32);
             pointCy3.TabIndex = 18;
             pointCy3.TextChanged += textBox3_TextChanged;
             // 
             // pointBy2
             // 
             pointBy2.Font = new Font("Segoe UI", 14F);
-            pointBy2.Location = new Point(382, 168);
+            pointBy2.Location = new Point(334, 126);
+            pointBy2.Margin = new Padding(3, 2, 3, 2);
             pointBy2.Name = "pointBy2";
-            pointBy2.Size = new Size(92, 39);
+            pointBy2.PlaceholderText = "y2";
+            pointBy2.Size = new Size(81, 32);
             pointBy2.TabIndex = 19;
             pointBy2.TextChanged += pointBy2_TextChanged;
             // 
             // Algorithm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1085, 504);
+            ClientSize = new Size(741, 406);
             Controls.Add(pointBy2);
             Controls.Add(pointCy3);
             Controls.Add(pointDy4);
@@ -275,6 +295,7 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Algorithm";
             Text = "Algorithm";
             ResumeLayout(false);
